06a8243d20c4db2126e7a5f056a63ce7b14125c1
Phillip Mispagel <atawmicpm@gmail.com>
2014-01-28 17:46:11 -0800

    update

--- marketo.rb
+++ marketo.rb
@@ -1,10 +1,12 @@
-# This assumes that 
+#!/usr/bin/env ruby
+
+# This assumes constraint on the combination of id and name being unique
+
 # mysql> create database testdb;
 # mysql> GRANT ALL PRIVILEGES ON testdb.* TO 'user'@'localhost' IDENTIFIED BY 'passwd';
 # mysql> CREATE TABLE NAMES (id INTEGER NOT NULL, name CHAR(100) NOT NULL, value CHAR(100) NOT NULL, timestamp TIMESTAMP, PRIMARY KEY(id), UNIQUE(id, name));
 
-#!/usr/bin/ruby
-
+require 'rubygems'
 require 'mysql'
 require 'json'
 require 'open-uri'
